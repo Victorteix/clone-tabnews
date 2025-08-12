6e88ab7550303433873d3f30cd9bbba4b1f0fd54
Victor <victor.teixeira@akanewmedia.com>
2025-08-11 23:21:19 -0400

    Changing message again

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Duda, eu te amo! Amor da minha vida toda!</h1>
+    return <h1>Duda, eu te amo! Amor da minha vida toda! Vc eh a perfeicao do universo</h1>
 }
 
 export default Home;
